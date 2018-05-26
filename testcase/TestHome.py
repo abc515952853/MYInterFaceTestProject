80abe0450b97d767c5617b213e0755fccf61316b
abc515952853 <123456789@qq.com>
2018-05-26 21:47:00 +0800

    首页

---
+++ testcase/TestHome.py
@@ -0,0 +1,22 @@
+import unittest
+import ddt
+from common import ReadExcl
+import ReadConfig 
+import requests
+import  json 
+
+class TestHome(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        print ("TestHome Start")
+    
+    @classmethod
+    def tearDownClass(cls):
+        print("TestHome Stop")
+
+    def test_Account(self):
+        readconfig=ReadConfig.ReadConfig()
+        headers = {"Content-Type":"application/json","Authorization":readconfig.get_member("session")} 
+        r = requests.get(url='http://api.hhx.qianjifang.com.cn/api/Home',headers = headers)
+        print(r.status_code,r.json())
+        self.assertEqual(200,r.status_code)
